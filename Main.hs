bf62c25e3f9ed208ca5ccba2e97211cbcc32a868
Stu Sherwin <stu.sherwin@gmail.com>
2014-02-24 00:02:45 +0000

    Add more planets, fix window reshape

--- Main.hs
+++ Main.hs
@@ -12,19 +12,34 @@ import Graphics
 
 deltaTimeResolution = 0.01
 
-sun     = Body { col = T.Yellow, rad = 6.96e8, mass = 1.98e30, pos = Vec 0 0,                    vel = Vec 0 0,                    acc = Vec 0 0 }
-jupiter = Body { col = T.Red,    rad = 6.99e7, mass = 1.89e27, pos = Vec (-7.78e11) 0,           vel = Vec 0 (-1.3e4),             acc = Vec 0 0 }
-earth   = Body { col = T.Blue,   rad = 6.37e6, mass = 5.97e24, pos = Vec 1.49e11 0,              vel = Vec 0 2.97e4,               acc = Vec 0 0 }
-moon    = Body { col = T.White,  rad = 1.73e6, mass = 7.34e22, pos = pos earth |+| Vec 3.84e8 0, vel = vel earth |+| Vec 0 1.02e3, acc = Vec 0 0 }
+sun     = Planet { colour = T.Yellow, radius = 6.96e8, mass' = 1.98e30, distance = 0,       speed = 0 }
+mercury = Planet { colour = T.Red,    radius = 2.43e6, mass' = 3.30e23, distance = 5.79e10, speed = 4.78e4 }
+venus   = Planet { colour = T.Yellow, radius = 6.05e6, mass' = 4.86e24, distance = 1.08e11, speed = 3.50e4 }
+earth   = Planet { colour = T.Blue,   radius = 6.37e6, mass' = 5.97e24, distance = 1.49e11, speed = 2.97e4 }
+moon    = Planet { colour = T.White,  radius = 1.73e6, mass' = 7.34e22, 
+                     distance = (distance earth) + 3.84e8, speed = (speed earth) + 1.02e3 }
+mars    = Planet { colour = T.Red,    radius = 3.40e6, mass' = 6.42e23, distance = 2.27e11, speed = 2.40e4 }
+jupiter = Planet { colour = T.Red,    radius = 6.99e7, mass' = 1.89e27, distance = 7.78e11, speed = 1.30e4 }
+saturn  = Planet { colour = T.Yellow, radius = 6.03e7, mass' = 5.68e26, distance = 1.43e12, speed = 9.69e3 }
 
-data State = State { planets :: [Body]
+data Planet = Planet { colour   :: T.Colour
+                     , radius   :: Double
+                     , mass'    :: Double
+                     , distance :: Double
+                     , speed    :: Double   }
+
+fromPlanet :: Planet -> Body
+fromPlanet Planet { colour = c, radius = r, mass' = m, distance = d, speed = s }
+  = Body { col = c, rad = r, mass = m, pos = Vec d 0, vel = Vec 0 s, acc = Vec 0 0 }
+
+data State = State { planets               :: [Body]
                    , distanceScaleExponent :: Double
-                   , origin :: (Double, Double)
-                   , paused :: Bool
-                   , track :: Maybe Int }
+                   , origin                :: (Double, Double)
+                   , paused                :: Bool
+                   , track                 :: Maybe Int        }
 
 state :: State
-state = State { planets = [sun, jupiter, earth, moon] 
+state = State { planets = map fromPlanet [sun, mercury, venus, earth, moon, mars, jupiter, saturn] 
               , distanceScaleExponent = -12
               , origin = (0, 0)
               , paused = False
@@ -51,22 +66,19 @@ display state' = do
   mapM_ (draw (findOrigin state) scaleFactor) $ planets state
   flush
   where
-    findOrigin State { origin = o, track = Nothing } = o
+    findOrigin State { origin = o,       track = Nothing } 
+      = o
     findOrigin State { origin = (xo,yo), track = Just t, planets = p }
-      | t >= 0 && t < length p = fromVector ( (Vec xo yo) |-| (pos (p !! t)))
-      | otherwise              = fromVector ( (Vec xo yo) |-| (pos (p !! 0)))
+      = fromVector ( (Vec xo yo) |-| (pos (p !! t)))
 
 reshape :: ReshapeCallback
 reshape (Size w h) = do
   viewport $= (Position 0 0, Size w h)
-  let width = fromIntegral w
-  let height = if h == 0 then 1.0 else fromIntegral h
-  let aspect = width / height
+  let wp = (fromIntegral w) / 300.0
+  let hp = (fromIntegral h) / 300.0
   matrixMode $= Projection
   loadIdentity
-  if width >= height
-    then ortho2D ((-1.0) * aspect) (1.0 * aspect) (-1.0) 1.0
-    else ortho2D (-1.0) 1.0 ((-1.0) / aspect) (1.0 / aspect)
+  ortho2D (-wp) wp (-hp) hp
   postRedisplay Nothing
 
 idle :: IORef DiffTime -> IORef State -> IdleCallback
@@ -91,13 +103,15 @@ keyboardMouse state' key Down _ _ = do
   state <- get state'
   let scaleFactor = 10.0 ** (-(distanceScaleExponent state))
   case key of
-    (Char '=') -> state' $~! (\s@State { distanceScaleExponent = e } -> s { distanceScaleExponent = e + 0.1 })
-    (Char '-') -> state' $~! (\s@State { distanceScaleExponent = e } -> s { distanceScaleExponent = e - 0.1 })
     (SpecialKey KeyUp)    -> state' $~! (\s@State { origin = (x,y)} -> s { origin = (x, y + 0.1 * scaleFactor) })
     (SpecialKey KeyDown)  -> state' $~! (\s@State { origin = (x,y)} -> s { origin = (x, y - 0.1 * scaleFactor) })
     (SpecialKey KeyLeft)  -> state' $~! (\s@State { origin = (x,y)} -> s { origin = (x - 0.1 * scaleFactor,y) })
     (SpecialKey KeyRight) -> state' $~! (\s@State { origin = (x,y)} -> s { origin = (x + 0.1 * scaleFactor,y) })
+    (Char '=') -> state' $~! (\s@State { distanceScaleExponent = e } -> s { distanceScaleExponent = e + 0.1 })
+    (Char '-') -> state' $~! (\s@State { distanceScaleExponent = e } -> s { distanceScaleExponent = e - 0.1 })
     (Char ' ') -> state' $=! state { paused = not (paused state) }
-    (Char num) | num >= '0' && num <= '9' -> state' $=! state { track = Just $ ord num - ord '0', origin = (0,0) }
+    (Char num) | num >= '0' && num <= '9' && trackPlanet < length (planets state) 
+      -> state' $=! state { track = Just trackPlanet, origin = (0,0) }
+        where trackPlanet = ord num - ord '0'
     _ -> return ()
 keyboardMouse _ _ _ _ _ = return ()
--- Physics.hs
+++ Physics.hs
@@ -5,7 +5,7 @@ module Physics (
 import Data.List
 import Types
 
-timeScaleFactor = 31536000
+timeScaleFactor = 3153600.0 -- 1 sec = 1 year
 granularity = 0.001
 gravitationalConstant = 6.67384e-11
 
@@ -15,16 +15,15 @@ simulate = turnCrank
                        | otherwise = turnCrank (updateDynamics ps granularity) (t - granularity)
 
 updateDynamics :: [Body] -> Double -> [Body]
-updateDynamics particles dt = zipWith4 update particles as vs xs
+updateDynamics bodies dt = zipWith4 (\p a v x -> p { pos = x, vel = v, acc = a}) bodies as vs xs
   where 
     scaledDt = dt * timeScaleFactor
-    as = gravitationalField $ map (\p -> (pos p, mass p)) particles
-    vs = zipWith (\a v -> v |+| (a |*| scaledDt)) as $ map vel particles
-    xs = zipWith (\v x -> x |+| (v |*| scaledDt)) vs $ map pos particles
-    update p a v x = Body { col = col p, rad = rad p, mass = mass p, pos = x, vel = v, acc = a }
+    as = gravitationalField $ map (\p -> (pos p, mass p)) bodies
+    vs = zipWith (\a v -> v |+| (a |*| scaledDt)) as $ map vel bodies
+    xs = zipWith (\v x -> x |+| (v |*| scaledDt)) vs $ map pos bodies
 
 gravitationalField :: [(Vec,Double)] -> [Vec]
-gravitationalField particles = map (\(p,m) -> fieldAt p $ delete (p,m) particles) particles
+gravitationalField bodies = map (\(p,m) -> fieldAt p $ delete (p,m) bodies) bodies
   where
     fieldAt posj is = (vecSum $ map (fieldContribution posj) is ) |*| (-gravitationalConstant)
     fieldContribution posj (posi, mi) = (unit $ posj |-| posi) |*| mi |/| absSquare (posi |-| posj)
